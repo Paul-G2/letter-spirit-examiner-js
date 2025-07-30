a92989d195a88e69edd498e64dea05c94e790b42
Paul-G2 <Paul-G2@users.noreply.github.com>
2025-07-29 21:36:07 -0400

    Add files via upload

--- LetterSpiritExaminer.js
+++ LetterSpiritExaminer.js
@@ -8582,34 +8582,35 @@ Namespace.HelpDialog = class extends Namespace.Dialog
 
         this.textDiv.innerHTML =
         '<p></p>' + 
-        '<p>This app is a JavaScript implementation of a computer model of human letter-recognition, ' +  
-        'that was originally developed by Douglas Hofstadter, Gary McGraw and John Rehling ' + 
+        '<p>This app implements a computer model of human letter-recognition that was invented by Douglas Hofstadter, Gary McGraw and John Rehling ' + 
         '(<a href="https://en.wikipedia.org/wiki/Fluid_Concepts_and_Creative_Analogies" target="_blank" rel="noopener noreferrer">link</a>).</p>' + 
         
         '<p>To try it out, enter a letter in the green left-hand grid, either by pressing the <i>random</i> button or by clicking on segments in the grid.</p>' +
         '<p>Then hit the <i>play</i> button to watch the Examiner "think." ' +  '(You can also pause, single-step, reset, and adjust the speed.)</p>' +
 
-        '<p>While running, the central work-area displays</p>' +
+        '<p>While running, the central white work-area displays</p>' +
         '<ul>' +
-            '<li> A summary of the Examiner&rsquo;s current actions, at the top</li>' +
-            '<li> A tentative breakdown of the input letter into color-coded parts, on the right side</li>' +
-            '<li> Descriptions for the parts, in the center' + 
+            '<li> A summary of the Examiner&rsquo;s current actions, at the top.</li>' +
+            '<li> A tentative breakdown of the input letter into color-coded parts, on the right side.</li>' +
+            '<li> Descriptions it has discovered for the parts, in the center.</li>' + 
         '</ul>' +
         
-        '<p>The thermometer shows how satisfied the Examiner is with its current progress; lower temperatures imply greater satisfaction.</p>' +
-
         '<p>In the yellow <i>Activations</i> area, the Examiner&rsquo;s built-in "concepts" are shown in a grid. ' + 
-        'The size of the dot over each one indicates how active that concept is at the current moment. (Red indicates negative activation.)</p>' +
+        'The size of the dot in each grid cell indicates how active that concept is at the current moment. (Red indicates negative activation.)</p>' +
 
         '<p>The pink <i>Coderack</i> area displays the list of subroutines or <i>codelets</i> that the Examiner ' +
         'uses to perform its work. The number of each type of codelet currently in the coderack is shown in a dynamical ' +
         'bar graph. The last-run codelet is indicated by a black triangle.</p>' +
-        
+
+        '<p>The thermometer shows how satisfied the Examiner is with its current progress; lower temperatures imply greater satisfaction.</p>' +
+
         '<p>The algorithm is probabilistic, so you may get different results each time you run it on a given letter.</p>' + 
 
         '<p><a href="https://github.com/Paul-G2/letter-spirit-examiner-js" target="_blank" rel="noopener noreferrer">This app&rsquo;s code</a> ' + 
-        'is heavily based on the original Scheme code by John Rehling, linked ' + 
-        '<a href="https://github.com/Alex-Linhares/FARGonautica/tree/master#project-letter-spirit" target="_blank" rel="noopener noreferrer">here</a>.</p>';
+        'is heavily based on the Scheme code of John Rehling, linked ' + 
+        '<a href="https://github.com/Alex-Linhares/FARGonautica/tree/master#project-letter-spirit" target="_blank" rel="noopener noreferrer">here</a>.</p>' +
+        
+        '</p>&nbsp;</p>';
     }
 
 };
